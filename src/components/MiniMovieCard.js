dab32e6e70377bceea5374118080939055fe1ef2
Neil Berg <neil.berg14@gmail.com>
2019-04-15 19:10:39 -0700

    Grid layout for MiniMovieCard

--- src/components/MiniMovieCard.js
+++ src/components/MiniMovieCard.js
@@ -7,38 +7,38 @@ import { faAngleDoubleRight, faStar } from '@fortawesome/free-solid-svg-icons';
 import { formatReleaseDate, createMovieSlug } from '../helper';
 
 const CardContainer = styled.div`
-  display: flex;
-  align-items: flex-end;
-  justify-content: space-between;
-  color: white;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-template-areas:
+    'title title'
+    'details link';
   border-bottom: 1px grey solid;
   padding: 0.5em 1em;
 
-  .info .title {
-    font-size: 1em;
+  .title {
+    grid-area: title;
     color: var(--green);
+    font-size: 1.1em;
     margin: 0;
-    padding: 0 0 0.25em 0;
+    padding: 0 0 0.5em 0;
   }
 
-  .info .score {
-    color: grey;
+  .details {
+    grid-area: details;
+    font-size: 0.9em;
+    color: lightgrey;
     margin: 0;
     padding: 0;
   }
 
-  .info .score .number {
-    font-size: 0.85em;
+  .details .number {
     color: white;
     padding-right: 0.35em;
   }
 
-  .info .date {
-    font-size: 0.9em;
-    color: lightgrey;
-  }
-
   .link {
+    grid-area: link;
+    justify-self: end;
     color: lightgrey;
     text-decoration: none;
     display: flex;
@@ -59,17 +59,15 @@ const CardContainer = styled.div`
 const MiniMovieCard = ({ movie, section }) => {
   return (
     <CardContainer>
-      <div className="info">
-        <p className="title">{movie.title}</p>
-        {section === 'now-playing' ? (
-          <p className="score">
-            <span className="number">{movie.vote_average}</span>
-            <FontAwesomeIcon icon={faStar} color="var(--yellow)" />
-          </p>
-        ) : (
-          <span className="date">{formatReleaseDate(movie.release_date)}</span>
-        )}
-      </div>
+      <p className="title">{movie.title}</p>
+      {section === 'now-playing' ? (
+        <p className="details">
+          <span className="number">{movie.vote_average}</span>
+          <FontAwesomeIcon icon={faStar} color="var(--yellow)" />
+        </p>
+      ) : (
+        <p className="details date">{formatReleaseDate(movie.release_date)}</p>
+      )}
       <Link className="link" to={createMovieSlug(movie)}>
         <span>Details</span>
         <FontAwesomeIcon icon={faAngleDoubleRight} />
