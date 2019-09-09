04acb8988cbd54bc22bb897ca634b96a8580af32
Neil Berg <neil.berg14@gmail.com>
2019-09-09 09:04:10 -0700

    formatting dates to YYYY-MM-DD

--- src/helper.js
+++ src/helper.js
@@ -4,21 +4,15 @@ import moment from 'moment';
 export const nowPlayingDates = () => {
   const currentDate = new Date(Date.now());
   const oneMonthAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
-  const currentDateStr = `${currentDate.getFullYear()}-${currentDate.getMonth() +
-    1}-${currentDate.getDate()}`;
-  const oneMonthAgoStr = `${oneMonthAgo.getFullYear()}-${oneMonthAgo.getMonth() +
-    1}-${oneMonthAgo.getDate()}`;
-
+  const currentDateStr = moment(currentDate).format('YYYY-MM-DD');
+  const oneMonthAgoStr = moment(oneMonthAgo).format('YYYY-MM-DD');
   return [oneMonthAgoStr, currentDateStr];
 };
 export const comingSoonDates = () => {
   const currentDate = new Date(Date.now());
   const oneMonthFromNow = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000);
-  const currentDateStr = `${currentDate.getFullYear()}-${currentDate.getMonth() +
-    1}-${currentDate.getDate()}`;
-  const oneMonthFromNowStr = `${oneMonthFromNow.getFullYear()}-${oneMonthFromNow.getMonth() +
-    1}-${oneMonthFromNow.getDate()}`;
-
+  const currentDateStr = moment(currentDate).format('YYYY-MM-DD');
+  const oneMonthFromNowStr = moment(oneMonthFromNow).format('YYYY-MM-DD');
   return [currentDateStr, oneMonthFromNowStr];
 };
 
